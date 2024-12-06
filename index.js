d950628c99bed74349c5dec5342d2952dd07fb1f
NazarKutoviy <147269741+NazarKutoviy@users.noreply.github.com>
2024-12-06 19:57:57 +0200

    фвв

---
+++ index.js
@@ -0,0 +1,18 @@
+// Task 1
+// const button = document.getElementById("changeButton");
+// const textInput = document.getElementById("textInput");
+// button.addEventListener("click", function () {
+//   button.textContent = textInput.value;
+// });
+// Task 2
+// const image = document.getElementById('image');
+// image.src = 'https://via.placeholder.com/400';
+// Task 3
+// const link = document.getElementById('link');
+// link.href = 'https://new-url.com';
+// const image = document.getElementById('image');
+// image.alt = 'Опис зображення';
+// Task 4
+const list = document.getElementById('list');
+const firstItem = list.querySelector('li');
+firstItem.textContent = 'Новий текст для першого елемента';
